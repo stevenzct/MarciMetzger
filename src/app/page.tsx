32d39f6113dd8902576b5baf847ae8a14a02b16d
stevenzct <stevencabugos138@gmail.com>
2025-10-20 11:30:50 +0800

    feat: add brandShowcase images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import About from "../../components/homepage/About";
+import BrandShowcase from "../../components/homepage/BrandShowcase";
 import Expertise from "../../components/homepage/Expertise";
 import Hero from "../../components/homepage/Hero";
 import SearchListings from "../../components/homepage/SearchListings";
@@ -10,6 +11,7 @@ export default function Home() {
       <About/>
       <Expertise/>
       <SearchListings/>
+      <BrandShowcase/>
     </div>
   );
 }
