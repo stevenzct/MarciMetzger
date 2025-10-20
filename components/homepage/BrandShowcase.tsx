ea7b3c55284f47129aa561fd5d341c10c858f0f4
stevenzct <stevencabugos138@gmail.com>
2025-10-20 11:28:56 +0800

    feat: add brandShowcase section

---
+++ components/homepage/BrandShowcase.tsx
@@ -0,0 +1,59 @@
+import styles from "./Hero.module.css";
+import Image from "next/image";
+
+const BrandShowcase = () => {
+  return (
+    <div
+      className={`${styles["hero-section"]} pt-[120px] pb-[120px] md:pb-auto md:pt-auto`}
+    >
+      <div className="container-wrapper w-full h-auto">
+        <div className="app-container lg:w-[75%] max-w-[1200px] w-auto mx-6 md:mx-12 lg:mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            <div className="">
+              <Image
+                src="/images/brandShowcase/1.jpg"
+                alt="brandShowcase"
+                className="rounded-[5px]"
+                width={285}
+                height={236}
+              />
+            </div>
+
+            <div className="">
+              <Image
+                src="/images/brandShowcase/2.png"
+                alt="brandShowcase"
+                className="rounded-[5px]"
+                width={285}
+                height={236}
+              />
+            </div>
+
+            <div className="">
+              <Image
+                src="/images/brandShowcase/3.jpg"
+                alt="brandShowcase"
+                className="rounded-[5px]"
+                width={285}
+                height={236}
+              />
+            </div>
+
+            <div className="">
+              <Image
+                src="/images/brandShowcase/4.jpg"
+                alt="brandShowcase"
+                className="rounded-[5px]"
+                width={285}
+                height={236}
+              />
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default BrandShowcase;
